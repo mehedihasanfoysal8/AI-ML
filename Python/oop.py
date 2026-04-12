0a9c069cddbf212a80ddcad5788ec7e56f10af5b
mehedihasanfoysal8 <mehedihasanfoysal10@gmail.com>
2026-04-12 13:34:47 +0600

    added oop

--- Python/oop.py
+++ Python/oop.py
@@ -6,6 +6,17 @@ class Car:
     def name_print(self):
         print("This is my name :", self.name)
 
+#Inheritance in python.....
+
+# Inheritance here. super function car er sob attribute and method 
+# er access niye niche. 
+# super function ke call korar jonno first a supper key word ta likht hoi.
+# then __init__(attribute ghuli pass korte hoi)
+
+class ElectricCar(Car):
+    def __init__(self, name, age, battery_size):
+        super().__init__(name, age)
+        self.battery_size = battery_size
 
 new_car = Car("Mehedi", 12)
 
